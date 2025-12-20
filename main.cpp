9807b6efc67a206288cf6ddea00af6026bfdf6f2
rathoreshur-gif <rathoreshur@gmail.com>
2025-12-21 03:37:59 +0530

    redid till addition

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include "matrix.h"
+#include <stdlib.h>
+using namespace std;
+
+
+int main(){
+	//Test for Addition
+	size_t numberOfRows;
+	size_t numberOfCols;
+	size_t dataA;
+	size_t dataB;
+	cout<<"Test for Addition of Matrices\n";
+	cout<<"Type number of rows:\n";
+	cin>> numberOfRows;
+	cout<<"Type number of columns:\n";
+	cin>> numberOfCols;
+
+	cout<<"The Matrix will contain "<< numberOfRows*numberOfCols <<" elements\n";
+	cout<<"Type a number containing "<< numberOfRows*numberOfCols <<" digits, the digits of the number will be stored in the matrixA\n";
+	cin>> dataA;
+	cout<<"Type a number containing "<< numberOfRows*numberOfCols <<"digits, the digits of the number will be stored in the matrixB\n";
+        cin>> dataB;
+
+	system("clear");
+	Matrix<int> matrixA(numberOfRows, numberOfCols);
+	for(int i=0; i<numberOfRows; i++)
+	{
+		for(int j=0; j< numberOfCols; j++)
+		{
+			matrixA.insertValue(i,j,dataA%10);
+			dataA = (dataA/10) - ((dataA%10)/10);
+		}
+	}	
+
+	Matrix<int> matrixB(numberOfRows, numberOfCols);
+        for(int i=0; i<numberOfRows; i++)
+        {
+                for(int j=0; j< numberOfCols; j++)
+                {
+                        matrixB.insertValue(i,j,dataB%10);
+                        dataB = (dataB/10) - ((dataB%10)/10);
+                }
+        }
+
+	Matrix<int> matrixC(numberOfRows, numberOfCols);
+	matrixC = matrixA + matrixB;
+	
+	cout<<"matrixA:\n";	
+	matrixA.print();
+	cout<<"matrixB:\n";
+        matrixB.print();
+	cout<<"matrixA + matrixB:\n";
+        matrixC.print();
+
+
+
+
+
+	return 0;
+};
+
+
--- matrix.h
+++ matrix.h
@@ -1,140 +1,91 @@
-#ifndef Matrix_h
-#define Matrix_h
+#pragma MATRIX_H
 
-#include <iostream>
-#include <vector>
-#include <stdexcept>
-#include <utility>
-template <typename Temp>
-class Matrix {
-	private: 
-		size_t rows;
-		size_t cols;
-		std::vector<std::vector<Temp>> data;
-
+#include <vector>;
+using namespace std;
+template <class T>
+class Matrix{
+	private:
+		T rows;
+		T cols;
+		vector<vector<T>> data;
 	public:
-		Matrix() : rows(0), cols(0) {}
+	        //constructor 
+		Matrix(size_t r, size_t c) : rows(r), cols(c)
+	        {
+	
 
-		Matrix(size_t rows, size_t cols) : rows(rows), cols(cols) 
-						   {
+		//resizing the vector of vector	
+		data.resize(rows);
+		for(int i=0; i<rows; i++)
+		{
+			data[i].resize(cols);
+		};
 
-				data.resize(rows, std::vector<Temp>(cols));}
-		Temp& operator()(size_t row, size_t col) {
-			if (row > rows-1 || col > cols-1) {
-				throw std::invalid_argument("Index does not exist");
+
+		//setting initial values to zero
+		for(int i=0; i<rows; i++)
+		{
+			for(int j=0; j<cols; j++)
+			{
+			data[i][j] = 0;
 			}
-			return data[row][col];}
-		const Temp& operator()(size_t row, size_t col) const {
-                        if (row > rows-1 || col > cols-1) {
-                                throw std::invalid_argument("Index does not exist");
-                        }
-                        return data[row][col];}
+		}
+	
+	
+		}
 
-		size_t numberOfRows() { return rows;}
-		size_t numberOfColumns() { return cols;}
-		void print() const {
-                        for (size_t i = 0; i < rows; i++){
-				for (size_t j=0; j< cols; j++){
-					std::cout << data[i][j] << "  ";
+	        //function: getValue
+		T getValue(size_t r, size_t c)
+		{
+			return data[r][c];
+		}
+
+		//function: insertValue
+		void insertValue(size_t r, size_t c, T value)
+		{
+			data[r][c] = value;
+		}
+
+		//function: print
+		void print()
+		{
+			for(int i=0; i<rows;i++)
+			{
+				for(int j=0; j<cols; j++)
+				{
+					cout<<data[i][j]<<"  ";
 				}
-				std::cout << "\n";
-			}}
+				cout<<'\n';
 
+			}
+			cout<<'\n';
+		}
 
-    Matrix operator+(const Matrix& other) const {
-        if (rows != other.rows || cols != other.cols) {
-            throw std::invalid_argument("Cannot add matrices");
-     }
+		//overloading the + operator
 
-        Matrix result(rows, cols);
-        for (size_t i = 0; i < rows; i++) {
-            for (size_t j = 0; j < cols; j++) {
-                result.data[i][j] = data[i][j] + other.data[i][j]; }}
-        return result; 
-    }
+		Matrix operator+(const Matrix &other)
+		{
+			Matrix result(rows,cols);
+			if(rows==other.rows && cols==other.cols)
+			{
 
-
-    Matrix operator*(Temp scalar) const {
-        Matrix result(rows, cols);
-
-        for (size_t i = 0; i < rows; i++) {
-            for (size_t j = 0; j < cols; j++) {
-                result.data[i][j] = data[i][j] * scalar; }}
-        return result;
-    }
-
-    Matrix operator*(const Matrix& other) const {
-        if (cols != other.rows) {
-            throw std::invalid_argument("Matrix multiplication dimension mismatch");
-        }
-
-        Matrix result(rows, other.cols);
-
-        for (size_t i = 0; i < rows; i++) {
-            for (size_t j = 0; j < other.cols; j++) {
-                Temp temp = 0;
-                for (size_t k = 0; k < cols; k++) {
-                    temp = temp + data[i][k]*other.data[k][j];
-                }
-                result(i, j) = temp;}}
-        return result;}
-
-    Matrix inverse() const {
-        if (rows != cols) {
-            throw std::invalid_argument("Matrix must be square");
-        }
-
-        size_t n = rows;
-        Matrix augmented(n, 2*n);
-
-        for (size_t i = 0; i<n; i++) {
-            for (size_t j = 0; j<n; j++) {
-                augmented(i,j) = data[i][j];
-            }
-            augmented(i,i+n) = 1;}
-
-        for (size_t i=0;i<n; i++) {
-            Temp pivot = augmented(i,i);
-
-            for (size_t j =0; j<2*n; j++) {
-                augmented(i,j) /= pivot;
-            }
-
-            for (size_t k=0; k<n; k++) {
-                if (k != i) {
-                    Temp factor = augmented(k,i);
-                    for (size_t j = 0; j < 2*n; j++) {
-                        augmented(k, j) -= factor*augmented(i, j);}}}}
-
-        Matrix result(n, n);
-        for (size_t i=0;i<n; i++) {
-            for (size_t j = 0; j < n; j++) {
-                result(i, j) = augmented(i, j + n);}}
-
-        return result;}
-
-
-    std::pair<Matrix, Matrix> lhDecomposition() const {
-        if (rows != cols) {
-            throw std::invalid_argument("Matrix must be square");
-        }
-
-        size_t n = rows;
-        Matrix L(n, n);
-        Matrix H = *this;
-
-        for (size_t i =0;i<n; i++) {
-            L(i, i) =1;
-        }
-	for (size_t i =0;i<n; i++) {
-            for (size_t k = i + 1; k<n; k++) {
-                Temp factor = H(k, i) / H(i, i);
-                L(k, i) = factor;
-                for (size_t j =i; j<n; j++) {
-                    H(k, j) -= factor * H(i, j);}}}
-
-        return {L, H};}
-
+				for(int i=0; i< rows; i++)
+				{
+					for(int j=0; j<cols; j++)
+					{
+						result.data[i][j] = data[i][j] + other.data[i][j];
+					}
+				}
+			}
+		//	else{
+			// exception handling will write while writing main.cpp
+		//	}
+		return result;
+		}
 
 };
-#endif
+
+
+
+
+
